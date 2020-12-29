a586aab34ecbd024074156ee1a33e4ec36bab718
FelixEIGato <tcin@outlook.es>
2020-12-29 00:36:19 -0500

    Se ha creado el controlador y  modelo de antenas

---
+++ Controlador/getAntena.php
@@ -0,0 +1,40 @@
+<?php
+
+    include_once("controlAntena.php");
+    $objAntena = new controlAntena;
+
+
+    $idAntena       = (isset($_POST['idAntena'])) ? $_POST['idAntena'] : '';
+    $nombreAntena   = (isset($_POST['nombreAntena'])) ? $_POST['nombreAntena'] : '';
+    $ip             = (isset($_POST['ip'])) ? $_POST['ip'] : '';
+    $mac            = (isset($_POST['mac'])) ? $_POST['mac'] : '';
+    $frecuencia     = (isset($_POST['frecuencia'])) ? $_POST['frecuencia'] : '';
+    $canal          = (isset($_POST['canal'])) ? $_POST['canal'] : '';
+    $idServidor     = (isset($_POST['idServidor'])) ? $_POST['idServidor'] : '';
+    $idTorre        = (isset($_POST['idTorre'])) ? $_POST['idTorre'] : '';
+    $idTipo         = (isset($_POST['idTipo'])) ? $_POST['idTipo'] : '';
+    $option         = (isset($_POST['option'])) ? $_POST['option'] : '';
+   
+
+    switch ($option) {
+        case 1:
+            $data = $objAntena->ingresarAntena($nombreAntena,$ip,$mac,$frecuencia,$canal,$idServidor,$idTorre,$idTipo);
+            print json_encode($data, JSON_UNESCAPED_UNICODE);
+            break;
+        case 2:
+            $data = $objAntena->actualizarAntena($idAntena,$nombreAntena,$ip,$mac,$frecuencia,$canal,$idServidor,$idTorre,$idTipo);
+            print json_encode($data, JSON_UNESCAPED_UNICODE);
+            break;
+        
+        case 3:
+            $data = $objAntena->eliminarAntena($idAntena);
+            print json_encode($data, JSON_UNESCAPED_UNICODE);
+            break;
+        
+        case 4:
+            $data = $objAntena->listarAntena();
+            print json_encode($data, JSON_UNESCAPED_UNICODE);
+            break;
+            
+    }
+?>
